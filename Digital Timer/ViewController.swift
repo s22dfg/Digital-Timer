2f0d8cbf0d5eda7d41b81873d8f20cb8bcf941e5
D7703_19 <d7703_19@D7703-19ui-iMac.local>
2018-04-11 11:45:57 +0900

    time

--- Digital Timer/ViewController.swift
+++ Digital Timer/ViewController.swift
@@ -10,11 +10,22 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var timeLabel: UILabel!
+    
+    var myTimer = Timer()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        myTimer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(updateTime), userInfo: nil, repeats: true)
     }
 
+    @objc func updateTime() {
+        let date = Date()
+        let formatter = DateFormatter()
+        formatter.dateFormat = "HH:mm:ss EE YYYY"
+        timeLabel.text = formatter.string(from: date)
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
