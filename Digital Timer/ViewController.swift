64f4c95cdad3337d1e2230e5c85f6037987c827d
D7703_19 <d7703_19@D7703-19ui-iMac.local>
2018-04-11 12:18:16 +0900

    주석 정리

--- Digital Timer/ViewController.swift
+++ Digital Timer/ViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    // timerSwitch는 한개는 아울렛 한개는 액션
     @IBOutlet var timeLabel: UILabel!
     @IBOutlet var timerSwitch: UISwitch!
     
@@ -17,10 +17,9 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        /*
+    /*
          myTimer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(updateTime), userInfo: nil, repeats: true)
-         */
+    */
         timerSwitch.setOn(false, animated: true)
     }
 
@@ -33,10 +32,10 @@ class ViewController: UIViewController {
         timeLabel.text = formatter.string(from: date)
     }
     
+    // 두번째 사용 스위치 액션으로 두기
     @IBAction func timerStarter(_ sender: Any) {
-        
+        // 클로저(closure)의 사용
         if timerSwitch.isOn == true {
-            // 클로저(closure)의 사용
             myTimer = Timer.scheduledTimer(withTimeInterval: 1, repeats: true, block: {(myTimer) in self.updateTime()})
         } else {
             myTimer.invalidate()
